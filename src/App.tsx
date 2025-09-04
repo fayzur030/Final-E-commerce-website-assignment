27e5f72363b177909567525c4fe3a2d10ad5c8c3
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2025-09-04 13:35:03 +0600

    update loginpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header'
+import LoginPage from './Pages/LoginPage'
 
 function App() {
+  const location = useLocation()
+  const hideHeaderRoutes = ['/login']
+  const shouldHideHeader = hideHeaderRoutes.includes(location.pathname)
   return (
     <>
-      <Header /> 
+      <div className='min-h-screen flex flex-col'>
+        {!shouldHideHeader && <Header />}
+
+        <main className='flex-grow'>
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+          <Outlet />
+        </main>
+      </div>
     </>
   )
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,9 +2,10 @@ import { CircleArrowRight, Mail, Phone } from 'lucide-react'
 import LanguageDropdown from './LanguageDropdown'
 import header_logo from '../assets/header-logo.svg'
 import { IoIosSearch } from 'react-icons/io'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
-    <header className='border-b bg-white'>
+    <header className=' bg-white'>
       {/* Top bar */}
       <div className='max-w-7xl mx-auto px-3 py-2 flex justify-between items-center text-sm'>
         <div className='flex gap-4 items-center'>
@@ -19,25 +20,24 @@ const Header = () => {
           </div>
         </div>
         {/* LOGIN */}
-        <div className='flex items-center gap-1 cursor-pointer'>
+        <Link to={'/login'} className='flex items-center gap-1 cursor-pointer'>
           <CircleArrowRight size={16} /> <span>LOGIN</span>
-        </div>
+        </Link>
       </div>
 
       {/* Main Navbar */}
       <nav className='border-t'>
-        <div className='max-w-7xl mx-auto px-6 py-3 flex justify-between items-center'>
+        <div className='max-w-7xl mx-auto px-6 py-3 flex justify-between items-center flex-col md:flex-row md:items-center md:justify-between gap-3 '>
           <img src={header_logo} alt='' className='h-10 w-auto' />
-          <div className='relative w-10/12'>
+          <div className='relative w-full md:w-10/12'>
             <input
               type='text'
               placeholder='Search Here'
               className='border w-full rounded-md bg-gray-100 px-4 py-2 focus:outline-none'
             />{' '}
-            <IoIosSearch
-              className='absolute right-3 top-1/2 -translate-y-1/2 text-gray-500'
-              size={20}
-            />
+            <div className='absolute right-0 top-1/2 -translate-y-1/2 bg-purple-900 p-3 rounded-r-lg cursor-pointer'>
+              <IoIosSearch className='text-white' size={20} />
+            </div>
           </div>
         </div>
       </nav>
